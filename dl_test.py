1c7d89ddbd5917793abde5d658f20e6737e59ed6
jackred <jackred@tuta.io>
2022-05-09 19:32:03 +0100

    ADD some basic ml on histology image of breast cancer
    acc 70% on basic thingy

--- dl_test.py
+++ dl_test.py
@@ -7,9 +7,12 @@ from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import train_test_split
 import dl_helper as dlh
 import torch
+import numpy as np
 import ml_helper as mlh
-
+from PIL import Image
 from sklearn.metrics import accuracy_score
+from skimage.color import rgb2gray
+
 
 def get_hyperparameter(model):
     loss_fn = torch.nn.CrossEntropyLoss()
@@ -22,12 +25,62 @@ def get_hyperparameter(model):
 
 
 def one():
+    """
+    data from the Labeled Faces in the Wild (lfw) dataset
+    """
     fw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
-    data = fw_people.images.reshape(1288, 1, 50, 37)
+    data = fw_people.images.reshape(len(fw_people.data), 1, 50, 37)
     label = fw_people.target
     x_train, x_test, y_train, y_test = train_test_split(
         data, label)
     layer_1 = dlh.create_default_2d_layer()
-    pred = dlh.train_and_test(x_train, y_train, x_test, layer_1, 7,
+    pred = dlh.train_and_test(x_train, y_train, x_test, layer_1,
+                              len(fw_people.target_names),
+                              get_hyperparameter, dlh.DEVICE, epochs=20)
+    print(accuracy_score(y_test, pred))
+
+
+def read_histo_from_path(path):
+    print(f"read from {path}")
+    names = []
+    labels = []
+    for i in os.listdir(path):
+        tmp = os.listdir(path+'/'+i)
+        names.extend(tmp)
+        labels.extend([i] * len(tmp))
+    data = []
+    new_labels = []
+    for i, name in enumerate(names):
+        print(f"{i+1}/{len(names)}      ", end="\r")
+        read = np.array(Image.open(f"{path}/{labels[i]}/{name}"))
+        if read.shape == (50, 50, 3):
+            data.append(rgb2gray(read))
+            new_labels.append(int(labels[i][0]))
+    data = np.array(data)
+    labels = np.array(new_labels)
+    return data, labels
+
+
+def read_histo_small():
+    path = "../data/Breast Histopathology Images SMALL"
+    train_path = path+'/train'
+    test_path = path+'/test'
+    x_train, y_train = read_histo_from_path(train_path)
+    x_test, y_test = read_histo_from_path(test_path)
+    return x_train, y_train, x_test, y_test
+
+
+def reshape(x):
+    return x.reshape(x.shape[0], 1, *x.shape[1:])
+
+
+def two():
+    x_train, y_train, x_test, y_test = read_histo_small()
+    n_class = len(np.unique(y_train))
+    layer_1 = dlh.create_default_2d_layer()
+    x_train = reshape(x_train)
+    x_test = reshape(x_test)
+    pred = dlh.train_and_test(x_train, y_train, x_test, layer_1,
+                              n_class,
                               get_hyperparameter, dlh.DEVICE, epochs=20)
     print(accuracy_score(y_test, pred))
